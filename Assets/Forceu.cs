220de55a5440cba3db706eef2827421a91b4d154
Wietske Klein Ganseij GMD1A <kuroneko-nico@live.nl>
2016-11-07 21:26:39 +0100

    Unity Pinball
    "Completed" Pinball

---
+++ Assets/Forceu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Forceu : MonoBehaviour {
+
+    public Vector3 direction;
+    public Rigidbody ball;
+    public int force;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        direction = collision.contacts[0].point;
+        ball.AddForce(direction * force);
+    }
+	
+}
